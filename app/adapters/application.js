e435ee49db5e4db8089f6ad19a6a4da868914f04
mcavaioni <michele.cavaioni@gmail.com>
2015-12-11 10:53:37 -0500

    created route for listings

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  namespace:"api/v1"
+  namespace:"api/v1",
   host: "http://6f544f3e.ngrok.io"
   // host: "localhost:3000"
 });
