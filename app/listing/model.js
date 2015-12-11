8e41c3202e04955cd6cd0388b4de052c18d32167
jaygr1 <jrib6@yahoo.com>
2015-12-11 13:25:12 -0500

    listings/listing page

--- app/listing/model.js
+++ app/listing/model.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  description: DS.attr('string'),
   start_date: DS.attr('string'),
   finish_date: DS.attr('string'),
   price: DS.attr('string'),
